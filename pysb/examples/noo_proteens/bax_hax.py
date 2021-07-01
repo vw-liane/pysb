d0ef769d52f8b8f22dfbb544245536f95349c96b
vw-liane <42220158+vw-liane@users.noreply.github.com>
2021-06-30 19:33:08 -0500

    first version rules from article
    writing new rules from Panda 2020 Apoptosis article

---
+++ pysb/examples/noo_proteens/bax_hax.py
@@ -0,0 +1,20 @@
+## descriptive text from Panda 2020 apoptosis review article
+## https://ejmcm.com/article_3657.html
+## layout helped by https://github.com/LoLab-VU/pysb/blob/master/pysb/examples/bax_pore.py
+
+## hax is like an ax, and like hacking the cell
+##  ...  or are we hacking bax
+
+from __future__ import print_function
+from pysb import *
+
+##  *+* operator to represent complexation
+##  *|* operator to represent backward/forward reaction
+##  *>>* operator to represent forward-only reaction
+##  *%* operator to represent a binding interaction between two species
+
+Model()
+
+# the BAX, binding sites, diff site states
+# shell setup for future initiations of the BAX
+Monomer('BAX', ['t1', 't2'], {'t1' : ['actv', 'inactv']}, {'t2' : ['actv', 'inactv']} )  
